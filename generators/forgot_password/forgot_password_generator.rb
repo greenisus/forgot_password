5421ef6b9efe73d91fb0ac4ba9972310613fc5e3
Michael Mayo <greenisus@gmail.com>
2008-07-27 12:29:41 -0700

    dynamic class names

--- generators/forgot_password/forgot_password_generator.rb
+++ generators/forgot_password/forgot_password_generator.rb
@@ -17,11 +17,9 @@ class ForgotPasswordGenerator < Rails::Generator::NamedBase
     
     @rspec = has_rspec?
 
-    @user_model_name = args.shift || 'user'
-    @controller_name = args.shift || 'passwords'
-    # @user_model_name = 'user'
-    # @controller_name = 'passwords'
-
+    @user_model_name = (args.shift || 'user')
+    @controller_name = @name.pluralize || 'passwords'
+    
     # sessions controller
     base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
     @controller_class_name_without_nesting, @controller_file_name, @controller_plural_name = inflect_names(base_name)
